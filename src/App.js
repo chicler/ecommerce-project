a51766e7ff2e48f0173500adb40d6fe738c57b39
chicler <104471636+chicler@users.noreply.github.com>
2023-01-20 17:34:25 -0600

    Importacion Boostrap
    XD

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import { Routes, Route } from 'react-router-dom'
