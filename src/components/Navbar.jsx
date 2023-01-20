645250f40d282b2a3d4a0a54fadb3e5449017b10
chicler <104471636+chicler@users.noreply.github.com>
2023-01-20 17:15:36 -0600

    Intento desesperado de reparar el Navbar
    pls work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,10 @@ El Navbar y todo lo que contiene fue estilizado con Bootstrap */
 export const Navbar = () => {
     return (
         <div>
+            <head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" />
+            </head>
             <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-light bg-light py-3 shadow-sm">
                 <div className="container">
                     <NavLink id='logo' className="navbar-brand fw-bold fs-12" to="/">DA SHOP</NavLink>
